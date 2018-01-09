32a3e9c49713075c691af94cf29a9a994524343b
Danis_Alifian <danisalifian@gmail.com>
2018-01-09 13:09:16 +0700

    P2.1
    Update Database

--- DB_Hotel.sql
+++ DB_Hotel.sql
@@ -30,6 +30,8 @@ CREATE TABLE `kamar` (
 
 /*Data for the table `kamar` */
 
+insert  into `kamar`(`id_kamar`,`tipe_kamar`,`harga`,`fasilitas`) values ('101','deluxe',2300000,'TV,Water heater'),('232','Superior',3000000,'sdlfkj ask'),('303','deluxe',300000,'kajdh'),('404','deluxe',440,'skfhjkdfhsf\nskdjfh\nsdfkjh'),('909','superior',700000,'akjdkj\nasdlj\nasldj');
+
 /*Table structure for table `konsumen` */
 
 DROP TABLE IF EXISTS `konsumen`;
@@ -37,6 +39,7 @@ DROP TABLE IF EXISTS `konsumen`;
 CREATE TABLE `konsumen` (
   `id_konsumen` varchar(15) NOT NULL,
   `nama` varchar(30) NOT NULL,
+  `j_kelamin` varchar(10) NOT NULL,
   `tgl_lahir` date NOT NULL,
   `tempat_lahir` varchar(40) NOT NULL,
   `alamat` text NOT NULL,
@@ -47,6 +50,8 @@ CREATE TABLE `konsumen` (
 
 /*Data for the table `konsumen` */
 
+insert  into `konsumen`(`id_konsumen`,`nama`,`j_kelamin`,`tgl_lahir`,`tempat_lahir`,`alamat`,`kota`,`no_telp`) values ('5656565656','hfgfh','Laki-laki','2018-01-08','sdjkhk','kjadh','kajh','dkj'),('9898989898','ahd','Perempuan','2018-01-08','d','s','gh','djh');
+
 /*Table structure for table `pegawai` */
 
 DROP TABLE IF EXISTS `pegawai`;
@@ -54,14 +59,18 @@ DROP TABLE IF EXISTS `pegawai`;
 CREATE TABLE `pegawai` (
   `id_pegawai` varchar(10) NOT NULL,
   `nama` varchar(30) NOT NULL,
+  `j_kelamin` varchar(10) NOT NULL,
   `tgl_lahir` date NOT NULL,
   `tempat_lahir` varchar(40) NOT NULL,
   `alamat` text NOT NULL,
   `no_telp` varchar(12) NOT NULL,
+  `pass` varchar(12) NOT NULL,
   PRIMARY KEY (`id_pegawai`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 /*Data for the table `pegawai` */
+
+insert  into `pegawai`(`id_pegawai`,`nama`,`j_kelamin`,`tgl_lahir`,`tempat_lahir`,`alamat`,`no_telp`,`pass`) values ('0987654321','jdfghjdf','Perempuan','2017-12-29','dhj','asjg','298478','12'),('12','hakjd','Laki-laki','2017-12-29','sdfkjh','akjdh','akdjh','qw'),('1234567890','sdkjhk','Perempuan','2018-01-08','mnmjk','kjhkj','98089080','admin');
 
 /*Table structure for table `pembayaran` */
 
@@ -95,9 +104,9 @@ DROP TABLE IF EXISTS `reservasi`;
 CREATE TABLE `reservasi` (
   `id_reservasi` varchar(10) NOT NULL,
   `id_konsumen` varchar(10) NOT NULL,
-  `tgl_reservasi` datetime NOT NULL,
-  `check_in` datetime NOT NULL,
-  `check_out` datetime NOT NULL,
+  `tgl_reservasi` date NOT NULL,
+  `check_in` date NOT NULL,
+  `check_out` date NOT NULL,
   `id_kamar` varchar(4) NOT NULL,
   `hari` int(11) NOT NULL,
   `catatan` text NOT NULL,
@@ -112,6 +121,8 @@ CREATE TABLE `reservasi` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 /*Data for the table `reservasi` */
+
+insert  into `reservasi`(`id_reservasi`,`id_konsumen`,`tgl_reservasi`,`check_in`,`check_out`,`id_kamar`,`hari`,`catatan`,`id_pegawai`) values ('249879','5656565656','2018-01-17','2018-01-02','2018-01-08','404',2,'lkjlkj','0987654321'),('834893274','9898989898','2018-01-12','2018-01-01','2018-01-08','303',3,'sdf','1234567890'),('893573','5656565656','2018-01-08','2018-01-19','2018-01-02','909',2,'sdfsdf\r\n','1234567890');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
